405b2a2e9128f395b74edfdf22ca047a614f8763
NAWNEET KUAMR GUPTA <159549550+nawneetgupta7479@users.noreply.github.com>
2025-07-26 23:14:41 +0530

    setup of LOLChat

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>this is frontend part of LOLChat</h1>
+     
+      </div>
+  );
+}
